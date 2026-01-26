5e52034fac620b6129e8d603cbd48d1ef7e631a0
Eric Peterson <eric@elpete.com>
2026-01-26 06:59:47 -0500

    Split file templates into BoxLang Class and BoxLang Script
    - Renamed 'BoxLang Script.bx' template to 'BoxLang Class.bx' for class files
    - Added new 'BoxLang Script.bxs' template for script files
    - Updated CreateFileAction to offer both Class and Script options
    - Cleaned up template formatting (removed comment from Template.bxm)

--- src/main/java/com/ortussolutions/intellijboxlang/actions/BoxLangCreateFileAction.java
+++ src/main/java/com/ortussolutions/intellijboxlang/actions/BoxLangCreateFileAction.java
@@ -9,6 +9,7 @@ import com.ortussolutions.intellijboxlang.BoxLangIcons;
 import org.jetbrains.annotations.NotNull;
 
 public final class BoxLangCreateFileAction extends CreateFileFromTemplateAction implements DumbAware {
+    private static final String CLASS_TEMPLATE_NAME = "BoxLang Class";
     private static final String SCRIPT_TEMPLATE_NAME = "BoxLang Script";
     private static final String TEMPLATE_TEMPLATE_NAME = "BoxLang Template";
 
@@ -20,6 +21,7 @@ public final class BoxLangCreateFileAction extends CreateFileFromTemplateAction 
     protected void buildDialog(@NotNull Project project, @NotNull PsiDirectory directory,
                                @NotNull CreateFileFromTemplateDialog.Builder builder) {
         builder.setTitle("New BoxLang File")
+            .addKind("BoxLang Class", BoxLangIcons.FILE, CLASS_TEMPLATE_NAME)
             .addKind("BoxLang Script", BoxLangIcons.FILE, SCRIPT_TEMPLATE_NAME)
             .addKind("BoxLang Template", BoxLangIcons.FILE, TEMPLATE_TEMPLATE_NAME);
     }
